9d8b63e9e9016a24eccd5efb97f963aef262f776
Theo Foucher <theonicolas.foucher@gmail.com>
2023-12-12 16:27:05 +0100

    Variables de la fonction définies

--- Exercice2.py
+++ Exercice2.py
@@ -1,10 +1,10 @@
 def afficher_longueur_mot(mot):
-    longueurs = []
+    longueur_tab = []
     while mot.lower() not in ["stop"]:
         longueur = len(mot)
-        longueurs.append(longueur)
+        longueur_tab.append(longueur)
         mot = input("Entrez un mot (écrire STOP pour arrêter): ")
-    print(longueurs)
+    print(longueur_tab)
     print("Programme terminé.")
 
 premier_mot = input("Entrez un mot (écrire STOP pour arrêter): ")
